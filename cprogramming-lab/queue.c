9cd5c13823e164a18fd7815b43b254386f5b3a3f
Robert <rbt61690@gmail.com>
2023-04-04 17:10:55 -0700

    mostly working

--- cprogramming-lab/queue.c
+++ cprogramming-lab/queue.c
@@ -74,32 +74,43 @@ bool q_insert_head(queue_t *q, int v)
  */
 bool q_insert_tail(queue_t *q, int v)
 {
-    /* You need to write the complete code for this function */
-    /* Remember: It should operate in O(1) time */
-
+  /* You need to write the complete code for this function */
+  /* Remember: It should operate in O(1) time */
+  list_ele_t *ele = malloc(sizeof(list_ele_t));
+  //happens if malloc returns NULL because there is no memory available
+  if (!ele) {
+    return false;
+  }
+  ele->value = v;
+  ele->next = NULL;
+  // set the element as the next item and then make it the tail
+  q->tail->next = ele;
+  q->tail = ele;
+  q->cnt++;
+  return true;
 }
 
 /*
   Attempt to remove element from head of queue.
   Return true if successful.
-  Return false if queue is NULL or empty.
+  *Return false if queue is NULL or empty.
   If vp non-NULL and element removed, store removed value at *vp.
   Any unused storage should be freed
 */
 bool q_remove_head(queue_t *q, int *vp)
 {
-    /* You need to fix up this code. */
-    if (!q || !q->head) {
-      return false;
-    }
-    list_ele_t *ele = q->head;
-    q->head = q->head->next;
-    if (vp) {
-      *vp = ele->value;
-    }
-    free(ele);
-    q->cnt--;
-    return true;
+  /* You need to fix up this code. */
+  if (!q || !q->head) {
+    return false;
+  }
+  list_ele_t *head = q->head;
+  q->head = q->head->next;
+  q->cnt--;
+  if (vp) {
+    *vp = head->value;
+  }
+  free(head);
+  return true;
 }
 
 /*
@@ -110,7 +121,10 @@ int q_size(queue_t *q)
 {
     /* You need to write the code for this function */
     /* Remember: It should operate in O(1) time */
-
+  if (!q || !q->head) {
+    return 0;
+  }
+  return q->cnt;
 }
 
 /*
@@ -122,7 +136,17 @@ int q_size(queue_t *q)
  */
 void q_reverse(queue_t *q)
 {
-    /* You need to write the code for this function */
-
+  /* You need to write the code for this function */
+  //WORK IN PROGRESS
+  list_ele_t *cur = q->head;
+  list_ele_t *next = q->head->next;
+  list_ele_t *new_next;
+  for(; next; cur = next, next = new_next) {
+    new_next = next->next;
+    next->next = cur;
+  }
+  q->head->next = NULL;
+  q->tail = q->head;
+  q->head = cur;
 }
 
